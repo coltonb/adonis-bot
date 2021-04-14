e5193f7d3c43546253daacb95ca5b73303ffec34
Colton Blake <14205996+coltonb@users.noreply.github.com>
2021-04-14 18:13:18 -0400

    use yargs-parser

--- bin/scripts/batch_rename_keys.js
+++ bin/scripts/batch_rename_keys.js
@@ -8,16 +8,18 @@ const redis = require("async-redis");
 const redisClient = redis.createClient({ url: process.env.REDISCLOUD_URL });
 
 async function batchRenameKeys() {
-  const keys = await redisClient.keys("text-predictor:*");
-  const multi = await redisClient.multi();
+  const keys = await redisClient.keys("text-predictor:words:*");
+  keys.push("text-predictor:dictionary");
+
+  const promises = [];
 
   for (key of keys) {
     const newKey = "text-predictor::" + key.slice(15);
-    multi.rename(key, newKey);
+    promises.push(redisClient.rename(key, newKey));
     console.log(`${key} => ${newKey}`);
   }
 
-  await multi.exec();
+  Promise.all(promises);
   console.log(`Renamed ${keys.length} keys.`);
 }
 
